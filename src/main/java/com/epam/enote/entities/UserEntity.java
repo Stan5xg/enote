6c058f6e5b4a52b6572a7455b5c55a95f1af98dc
SergeyAlbrant <albrants95@mail.ru>
2017-12-07 20:24:25 +0300

    created UserEntity

---
+++ src/main/java/com/epam/enote/entities/UserEntity.java
@@ -0,0 +1,32 @@
+package com.epam.enote.entities;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @NotEmpty
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotEmpty
+    @Column(name = "password_hash")
+    public String passwordHash;
+
+
+}

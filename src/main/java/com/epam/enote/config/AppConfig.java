d8f06fe2171ef7fb07c77576573cbc4e1c53d06e
Sergei Morozov <sudostan@gmail.com>
2017-12-08 20:08:40 +0300

    Database with SQL scripts created

---
+++ src/main/java/com/epam/enote/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.epam.enote.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.epam.enote"})
+public class AppConfig {
+}

280055be783a0814d1386b345e7a175e0a64cdc1
SergeyAlbrant <albrants95@mail.ru>
2017-12-11 20:00:56 +0300

    made User service interface

--- src/main/java/com/epam/enote/services/UserService.java
+++ src/main/java/com/epam/enote/services/UserService.java
@@ -1,4 +1,22 @@
 package com.epam.enote.services;
 
+import com.epam.enote.entities.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    void create(User user);
+
+    void update(User user);
+
+    void delete(User user);
+
+    void deleteById(int id);
+
+    User getById(int id);
+
+    User getByName(int id);
+
+    List<User> getAll();
 }
